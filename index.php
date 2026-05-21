de07f5a84af9ec16c145dd4e0cde449696d54810
Marsha Hadirah Bt Muhammad Hidzir <marshaisverycool@gmail.com>
2026-05-21 19:33:01 +0800

    index.php
    updated button

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
                     <div class="page-header clearfix">
                         <h2 class="pull-left">Employees Details</h2>
                         <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+                        <a href="./create.php" class="btn btn-success pull-right">Add New Employee</a>
                     </div>
                     <?php
                     // Include config file
